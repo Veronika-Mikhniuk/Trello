1ff3b7b6514fdd7319416ac3d893ed2644ddfe03
Vernika-Mikhniuk <veronika.mikhnuyk@gmail.com>
2024-08-22 00:51:42 +0300

    feat: added functionality for deleting particular task, also for limiting count in-progress tasks

--- src/js/app.js
+++ src/js/app.js
@@ -18,6 +18,7 @@ const deleteConfirmationButtonElement = document.querySelector('#deleteTasksConf
 addTaskFormElement.addEventListener('submit', handleSubmitFormAddTask)
 addTaskModalElement.addEventListener('hidden.bs.modal', handleCloseModalAddTask)
 tasksBlock.addEventListener('click', handleClickDropdownMove)
+tasksBlock.addEventListener('click', handleClickButtonDelete)
 deleteConfirmationButtonElement.addEventListener('click', handleClickButtonDeleteDone)
 
 
@@ -61,7 +62,15 @@ function handleClickDropdownMove({ target }) {
             if (target.classList.contains('dropdown-item-todo')) {
                 tasks[index].status = 'todo'
             } else if (target.classList.contains('dropdown-item-progress')) {
-                tasks[index].status = 'in-progress'
+                const progressTasksLength = tasks.filter(task => task.status == 'in-progress').length
+
+                if(progressTasksLength <= 5) {
+                    tasks[index].status = 'in-progress'
+                } else {
+                    const limitWarningModal = new Modal(document.querySelector('#taskLimitModal'))
+                    limitWarningModal.show()
+                }
+                
             } else {
                 tasks[index].status = 'done'
             }
@@ -70,6 +79,25 @@ function handleClickDropdownMove({ target }) {
             saveTasksToStorage(tasks)
             render(tasks)
         }
+    }
+}
+
+function handleClickButtonDelete({ target }) {
+    const tasks = getTasksFromStorage()
+
+    if (target.classList.contains('board__task-delete')) {
+
+        const { dataset: { taskId } } = target.closest('.board__task')
+
+        tasks.forEach((task, index) => {
+
+            if (task.id == taskId) {
+                tasks.splice(index, 1)
+            }
+
+            saveTasksToStorage(tasks)
+            render(tasks)
+        })
     }
 }
 
@@ -154,15 +182,15 @@ const buildTemplateTask = ({ title, description, user, createdAt, colorClass, id
                 </div>
                 <button class="btn btn-outline-secondary btn-sm board__task-edit" type="button">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                        class="bi bi-pencil-fill pointer-events-none" viewBox="0 0 16 16">
-                        <path class="pointer-events-none"
+                        class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                        <path
                             d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z" />
                     </svg>
                 </button>
                 <button class="btn btn-outline-danger btn-sm board__task-delete" type="button">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                        class="bi bi-trash3-fill pointer-events-none" viewBox="0 0 16 16">
-                        <path class="pointer-events-none"
+                        class="bi bi-trash3-fill" viewBox="0 0 16 16">
+                        <path
                             d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5" />
                     </svg>
                 </button>
