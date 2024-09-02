f60e0d3052c69a4016a68d9360a54e4116d83532
Vernika-Mikhniuk <veronika.mikhnuyk@gmail.com>
2024-09-02 17:27:23 +0300

    :feat: added new styles and devided app.scss into modules

--- src/js/app.js
+++ src/js/app.js
@@ -245,14 +245,14 @@ const buildTemplateTask = ({ title, description, userId, createdAt, colorClass, 
                         <li><a class="dropdown-item dropdown-item-done" href="#">Done</a></li>
                     </ul>
                 </div>
-                <button class="btn btn-outline-secondary btn-sm board__task-edit" type="button">
+                <button class="btn btn-sm board__task-edit" type="button">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                         class="bi bi-pencil-fill" viewBox="0 0 16 16">
                         <path
                             d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z" />
                     </svg>
                 </button>
-                <button class="btn btn-outline-danger btn-sm board__task-delete" type="button">
+                <button class="btn btn-danger btn-sm board__task-delete" type="button">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                         class="bi bi-trash3-fill" viewBox="0 0 16 16">
                         <path
